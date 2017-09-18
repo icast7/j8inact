4ad7be8758ea8fcd8e4358d3d219060d2f064776
ikaz <icastillejos@gmail.com>
2017-09-18 12:59:22 -0500

    Added runnables example

---
+++ src/main/java/ch3/LambdaRunnable.java
@@ -0,0 +1,23 @@
+package ch3;
+
+public class LambdaRunnable {
+
+    public static void process(Runnable r) {
+        r.run();
+    }
+
+    public static void main(String[] args) {
+        Runnable r1 = () -> System.out.println("Hello World 1");
+
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello World 2");
+            }
+        };
+
+        process(r1);
+        process(r2);
+        process(() -> System.out.println("Hello World 3"));
+    }
+}
