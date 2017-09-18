78804f7227a7bc703850e8d93084f72c11f48e3e
ikaz <icastillejos@gmail.com>
2017-09-17 20:07:59 -0500

    Added ex2

---
+++ src/main/java/ch2/Apple.java
@@ -0,0 +1,19 @@
+package ch2;
+
+public class Apple {
+    public int weight;
+    public String color;
+
+    public Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/ch2/MyAppleFormatters.java
@@ -0,0 +1,41 @@
+package ch2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MyAppleFormatters {
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(120, "red"));
+
+        prettyPrintApple(inventory, new AppleFancyFormatter());
+        prettyPrintApple(inventory, new AppleSimpleFormatter());
+    }
+
+    static class AppleFancyFormatter implements AppleFormatter {
+        @Override
+        public String accept(Apple a) {
+            String characteristic = a.getWeight() > 150 ? "heavy" : "light";
+            return "A " + characteristic + " " + a.getColor() + " apple.";
+        }
+    }
+
+    static class AppleSimpleFormatter implements AppleFormatter {
+        @Override
+        public String accept(Apple a) {
+            return "An apple of " + a.getWeight() + " g.";
+        }
+    }
+
+    public static void prettyPrintApple(List<Apple> inventory, AppleFormatter formatter) {
+        for (Apple apple : inventory) {
+            String output = formatter.accept(apple);
+            System.out.println(output);
+        }
+    }
+}
+
+
