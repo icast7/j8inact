e5497772287076d6adff81b24e3b22be522ff9fe
icaste <ignacio.castillejos@nike.com>
2017-10-18 17:28:33 -0700

    Added example of depleted stream

---
+++ src/main/java/ch4/StreamErrors.java
@@ -0,0 +1,21 @@
+package ch4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamErrors {
+    public static void main(String[] args) {
+        try {
+            List<String> title = Arrays.asList("Java8", "In", "Action");
+            Stream<String> s = title.stream();
+            System.out.println("1st time");
+            s.forEach(System.out::println);
+            Thread.sleep(1000);
+            System.out.println("\n2nd time");
+            s.forEach(System.out::println);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
