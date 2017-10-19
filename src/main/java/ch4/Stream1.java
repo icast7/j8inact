8b2577c298807ced7726ece20e119529941ce7ac
icaste <ignacio.castillejos@nike.com>
2017-10-18 17:16:48 -0700

    Added parallel stream example

--- src/main/java/ch4/Stream1.java
+++ src/main/java/ch4/Stream1.java
@@ -20,6 +20,14 @@ public class Stream1 {
         System.out.println("*** 300 Caloric Dished  ");
         System.out.println(threeHighCaloricDishNames);
 
+        List<String> parallelThreeHighCaloricDishNames = Dish.menu.parallelStream()
+                        .filter(d -> d.getCalories() > 300)
+                        .map(Dish::getName)
+                        .limit(3)
+                        .collect(toList());
+        System.out.println("*** Parallel 300 Caloric Dished  ");
+        System.out.println(parallelThreeHighCaloricDishNames);
+
         Map<Dish.Type, List<Dish>> dishesByType = Dish.menu.stream().collect(groupingBy(Dish::getType));
         System.out.println("***  Dishes grouped by type");
         System.out.println(gson.toJson(dishesByType));
