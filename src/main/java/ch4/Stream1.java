2776ba98ee04ec4c9954ea13649d4e8df9fdc5c4
icaste <ignacio.castillejos@nike.com>
2017-10-18 17:14:07 -0700

    Added Stream example

--- src/main/java/ch4/Stream1.java
+++ src/main/java/ch4/Stream1.java
@@ -1,16 +1,27 @@
 package ch4;
 
-import java.util.List;
+import com.google.gson.Gson;
 
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
 public class Stream1 {
+    final static Gson gson = new Gson();
+
     public static void main(String[] args) {
         List<String> threeHighCaloricDishNames = Dish.menu.stream()
                         .filter(d -> d.getCalories() > 300)
                         .map(Dish::getName)
                         .limit(3)
                         .collect(toList());
+        System.out.println("*** 300 Caloric Dished  ");
         System.out.println(threeHighCaloricDishNames);
+
+        Map<Dish.Type, List<Dish>> dishesByType = Dish.menu.stream().collect(groupingBy(Dish::getType));
+        System.out.println("***  Dishes grouped by type");
+        System.out.println(gson.toJson(dishesByType));
     }
 }
