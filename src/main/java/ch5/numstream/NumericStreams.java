2659bd16132b27549946bd6459363bb3dd548100
icaste <ignacio.castillejos@nike.com>
2017-11-13 15:00:14 -0800

    Working on numeric streams examples

--- src/main/java/ch5/numstream/NumericStreams.java
+++ src/main/java/ch5/numstream/NumericStreams.java
@@ -1,7 +1,16 @@
 package ch5.numstream;
 
+import ch4.util.Dish;
+
+import static ch4.util.Dish.menu;
+
 public class NumericStreams {
     public static void main(String[] args){
+        int caloriesMap = menu.stream().map(Dish::getCalories).reduce(0, Integer::sum);
+        System.out.println("All calories with Map: " + caloriesMap);
+
+        int caloriesMapInt = menu.stream().mapToInt(Dish::getCalories).sum();
+        System.out.println("All calories with MapInt: " + caloriesMapInt);
 
     }
 }
